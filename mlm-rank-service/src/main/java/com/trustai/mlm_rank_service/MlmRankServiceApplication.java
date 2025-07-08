13b65ac25d2adf1375803873f5852aab68b00bc2
arpan jana <arpangroup1@gmail.com>
2025-07-08 14:13:04 +0530

    rank-calculation-logic

---
+++ mlm-rank-service/src/main/java/com/trustai/mlm_rank_service/MlmRankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trustai.mlm_rank_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlmRankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlmRankServiceApplication.class, args);
+	}
+
+}
---
+++ mlm-rank-service/src/test/java/com/trustai/mlm_rank_service/MlmRankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustai.mlm_rank_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmRankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
