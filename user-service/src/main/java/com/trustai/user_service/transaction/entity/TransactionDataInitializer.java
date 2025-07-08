6ea9ba9857c3b8433232f1a367b73f47cc4fa52c
arpan jana <arpangroup1@gmail.com>
2025-07-08 14:00:06 +0530

    rank-calculation-logic

--- user-service/src/main/java/com/trustai/user_service/transaction/entity/TransactionDataInitializer.java
+++ user-service/src/main/java/com/trustai/user_service/transaction/entity/TransactionDataInitializer.java
@@ -4,6 +4,7 @@ import com.trustai.common.enums.TransactionType;
 import com.trustai.user_service.transaction.repository.TransactionRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
